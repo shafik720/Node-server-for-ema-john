2fba09efd8f745cbe99639a9a781b3f2a824e0b7
shafik720 <shafikrasel5@gmail.com>
2023-01-14 15:47:18 +0600

    setting up mongo db

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app =express();
+require('dotenv').config()
 
 app.use(express.json());
 app.use(cors());
 
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ydjbhxm.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        app.get('/', async(req, res)=>{
+            res.send('hello world');
+        })
+    }
+    finally{}
+}
+
+run().catch(console.dir);
 
 
 app.get('/', (req, res)=>{
